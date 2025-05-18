ba201829de7f4376dc12f476fe609a64590d0164
dhoh <dhoh56@gmail.com>
2025-05-18 13:03:33 +0900

    sidebar menu 추가

--- flutter/test/widget_test.dart
+++ flutter/test/widget_test.dart
@@ -8,7 +8,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 
-import 'package:score/main.dart';
+import 'package:score/main_sample.dart';
 
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
